c23a74c8b5ee5bc02daf87a6d623f3febf863c91
Yutaro Kijima <eddix.ibanez@outlook.jp>
2023-11-23 18:20:06 +0900

    Improved chart style

--- src/libs/features/populationStatistics/PopulationsChart/index.tsx
+++ src/libs/features/populationStatistics/PopulationsChart/index.tsx
@@ -29,7 +29,7 @@ const populationByYear = (
   };
 };
 
-export const PopulationChart = ({ populations, populationLabel }: Props) => {
+const PopulationChart = ({ populations, populationLabel }: Props) => {
   const selected = usePrefectureSelect((state) => state.selected);
   const prefCodes = selected.map((_) => _.code);
   const data = years.map((year) =>
@@ -38,13 +38,45 @@ export const PopulationChart = ({ populations, populationLabel }: Props) => {
 
   const width = useMemo(() => {
     if (typeof window === 'undefined') return 1000;
-    return Math.min(screen.width - 40, 1000);
+    return Math.min(screen.width * 0.9, 1000);
   }, []);
+  const isSp = width < 800;
+  const margin = isSp ? 10 : 50;
+  const height = isSp ? width * 0.8 : width / 2;
 
   return (
-    <LineChart data={data} width={width} height={width}>
+    <LineChart
+      data={data}
+      width={width}
+      height={height}
+      margin={{ top: margin + 50, left: margin, right: margin, bottom: margin + 50 }}
+    >
+      <XAxis dataKey="name" />
+      {!isSp && (
+        <YAxis
+          tick={({
+            payload,
+            tickFormatter,
+            visibleTicksCount,
+            verticalAnchor,
+            y,
+            ...props
+          }) => {
+            return (
+              <text {...props} y={y + 5}>
+                {payload.value / 10000}万人
+              </text>
+            );
+          }}
+        />
+      )}
       {selected.map((pref) => (
-        <Line key={pref.code} dataKey={String(pref.code)} stroke={pref.color} />
+        <Line
+          key={pref.code}
+          dataKey={String(pref.code)}
+          type="monotone"
+          stroke={pref.color}
+        />
       ))}
     </LineChart>
   );
