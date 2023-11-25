8000b210a6a660810bcfea73f2b7c4dec11788d7
Yutaro Kijima <eddix.ibanez@outlook.jp>
2023-11-25 14:27:30 +0900

    fix style

--- src/libs/features/populationStatistics/PrefectureSelector/PrefectureSelector.css.ts
+++ src/libs/features/populationStatistics/PrefectureSelector/PrefectureSelector.css.ts
@@ -1,9 +1,17 @@
 import { style } from '@vanilla-extract/css';
 
+import { queries } from '@/libs/styles/constants';
+
 export const checkBoxGrid = style({
   display: 'grid',
   gridTemplateColumns: 'repeat(auto-fill, 100px)',
   gap: '20px',
   maxWidth: '100%',
   marginBottom: '50px',
+  justifyContent: 'center',
+  '@media': {
+    [queries.desktop]: {
+      justifyContent: 'flex-start',
+    },
+  },
 });
